b5a33fc6f63444f0bf51de4041437bd122c235f9
Sajid <37099683+sajidalichunmun@users.noreply.github.com>
2022-04-02 10:38:47 +0530

    Update IBusinessLogic.php

--- app/Interfaces/IBusinessLogic.php
+++ app/Interfaces/IBusinessLogic.php
@@ -5,6 +5,7 @@ use Illuminate\Http\Request;
 interface IBusinessLogic
 {
     public function list();
+    public function show($id);
     public function store($request);
     public function update($request,$id);
     public function delete($id);
